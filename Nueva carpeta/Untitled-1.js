e1575ca7fb37125d4987282ad3af322b2bb65b36
Enyel900 <140356020+Enyel900@users.noreply.github.com>
2024-04-11 14:37:51 -0400

    Add files via upload

---
+++ Nueva carpeta/Untitled-1.js
@@ -0,0 +1,59 @@
+const body = document.body.childNodes[1];
+console.log(body);
+
+// firtChild retorna el primer nodo de un padre.
+// firtElementChild retorna nodo html.
+
+// lasChild retorna el ultimo nodo.
+// lasElementChild retorna el ultimo elemento.
+
+// hasChildNotes() retorna verdadero si el contenedo tiene hijos.
+
+// .nextSibling retorna al hermano
+
+// .nextElementsibling retorna un elemnto hermano.
+
+// .previousSibling retorna el anterior hermano.
+
+
+// .previousElementSibling  retorna el anterior hermano.
+
+// texcontent modificar html.
+
+// addEventListenner (´click´), funtion ()
+
+// document.style.lo que sea.
+
+// parentNode elegir al padre.
+
+
+// getElementById(), 
+// getElementsByClassName
+// getElementsByTagName
+// getElementsByName obtener el valor del input name solo se usa en dovcument
+// querySelector selecionador de Clasess.
+// .items obtener su elemeno por posicion.
+//
+
+
+// modificadores.
+
+
+
+// ClassName retorna un strig o el valor de la etiqueta
+
+// text content sobrescribir texto.
+
+
+//getattribute() obtiene el atributo de una etiqueta.
+
+// podemos llamar el atributo .id
+
+
+// setAttribute () agregar un atributo  atributos.
+
+// hasAtribute retorna un valor buleano si tiene el atributo.
+
+//removeAttribute()
+
+// 
---
+++ Nueva carpeta/body1.js
@@ -0,0 +1,1 @@
+const body1 = document.body;
